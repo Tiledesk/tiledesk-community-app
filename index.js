5ff9b74af95b829693e63e8a65c09d2d706de583
Andrea Sponziello <andrea.sponziello@gmail.com>
2023-02-15 19:35:56 +0100

    mongo connect

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ const templates = require("./templatesRoute");
 const templatesRoute = templates.router;
 app.use("/chatbots", templatesRoute);
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Tiledesk Community app listening on port ', port);
+
+mongoose.connect(process.env.mongoUrl, { "useNewUrlParser": true, "autoIndex": false }, async (err) => {
+  if (err) { 
+    console.error('(Tilebot) Failed to connect to MongoDB on ' + process.env.mongoUrl + " ", err);
+  }
+  else {
+    const port = process.env.PORT || 3000;
+    app.listen(port, function () {
+      console.log('Tiledesk Community app listening on port ', port);
+    });
+  }
 });
+
+
+
+
   
