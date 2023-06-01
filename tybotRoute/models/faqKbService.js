ee0fb336dca0c0a8704699de087a80499c3cc967
leomirco <leomirco@gmail.com>
2023-06-01 10:47:01 +0000

    ver 0.3 added query order by createdAt in Desc

--- tybotRoute/models/faqKbService.js
+++ tybotRoute/models/faqKbService.js
@@ -11,13 +11,13 @@ class FaqKbService {
         "trashed": { $in: [null, false] }
       }
     }
-    var sortQuery = {};
+    var sortQuery = { createdAt: -1 };
     sortQuery["score"] = -1;
     console.log("(Service) GET ALL FAQ_KBs");
     return new Promise((resolve, reject) => {
       // let query = {public: options.public, certified: options.certified};
-      Faq_kb.find(query).sort(sortQuery).lean().exec( (err, bots) => {
-        console.log(".....")
+      Faq_kb.find(query).sort(sortQuery).lean().exec((err, bots) => {
+        console.log("sortQuery", sortQuery)
         if (err) {
           console.error("err", err)
           reject(err);
